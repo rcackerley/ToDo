df2038fcbf1bc0500d201913c7a7b02e75b19530
Robby Ackerley <rcackerley@me.com>
2018-06-18 13:19:54 -0400

    Convert to MVC Design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Robby Ackerley on 6/18/18.
+//  Copyright © 2018 Robby Ackerley. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var checked = false
+}
